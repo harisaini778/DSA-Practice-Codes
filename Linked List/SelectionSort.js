3d9822e71a22e94dbd1f12888832215204611c9a
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-09-01 20:38:35 +0530

    added algorithm for the selection sort algorithm

---
+++ Linked List/SelectionSort.js
@@ -0,0 +1,32 @@
+class Sort {
+
+    SelectionSort(arr) {
+     
+        let n = arr.length;
+        for (let i = 0; i < n - 1; i++){
+
+            let minIndex = i; 
+            
+            for (let j = i + 1; j < n; j++){
+                
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+                
+
+            }
+            if (minIndex != i) {
+                let temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+
+        }
+        return arr;
+
+    }
+    
+}
+const obj = new Sort();
+obj.SelectionSort([11,1,5,9,12,4]);
+
