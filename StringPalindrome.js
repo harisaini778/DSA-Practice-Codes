05eb02bc34be4885af6b47514518c40690d0ccce
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-09-23 15:53:12 +0530

    constructed a code for cheking a given string is a Palindrome or not

---
+++ StringPalindrome.js
@@ -0,0 +1,25 @@
+
+const isPalindrome = (str) => {
+    const cleanStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    console.log(cleanStr);
+
+    if (cleanStr.length <= 1) {
+        return true;
+    }
+
+    let left = 0;
+    let right = cleanStr.length - 1;
+
+    while (left <= right) {
+        if (cleanStr[left] != cleanStr[right]) {
+            return false;
+        } else {
+            left++;
+            right--;
+        }
+    }
+
+    return true;
+}
+const inputStr = "Madam"; //A man, a plan, a canal, Panama
+console.log(isPalindrome(inputStr));
